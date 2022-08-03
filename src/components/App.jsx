952ad8b9236e5f701b59edfeb2f371f0b391d86d
nandhakumar18 <sangainandhu18@gmail.com>
2022-08-03 18:20:52 +0530

    added fog

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -39,6 +39,18 @@ function App() {
           )}
         </div>
       </main>
+      <div id="foglayer_01" className="fog">
+        <div className="image01"></div>
+        <div className="image02"></div>
+      </div>
+      <div id="foglayer_02" className="fog">
+        <div className="image01"></div>
+        <div className="image02"></div>
+      </div>
+      <div id="foglayer_03" className="fog">
+        <div className="image01"></div>
+        <div className="image02"></div>
+      </div>
     </>
   )
 }
